3a13706a0a640e3bc53c827179b18320d43f07b0
xmnlz <lemmeq9@gmail.com>
2023-11-28 05:17:05 +0100

    fix(lib): added check for null object for safe-json-parse

--- src/lib/safe-json-parse.ts
+++ src/lib/safe-json-parse.ts
@@ -1,6 +1,10 @@
+import { isEmptyObject } from './is-empty-obj.js';
+
 export const safeJsonParse = <T>(json: string, fallback: T) => {
   try {
-    return JSON.parse(json);
+    const parsedJson = JSON.parse(json);
+    if (isEmptyObject(parsedJson)) return fallback;
+    return parsedJson;
   } catch (e) {
     return fallback;
   }
