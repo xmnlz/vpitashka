2ed6281a80f2b8a225bda44f10af4ad5a1a701c8
xmnlz <lemmeq9@gmail.com>
2025-03-24 22:26:18 +0100

    refactor: improved error handling in safe-env

--- apps/bot/src/lib/safe-env.ts
+++ apps/bot/src/lib/safe-env.ts
@@ -1,9 +1,8 @@
+import { throws } from "assert";
 import { env } from "process";
 import { z } from "zod";
 
 const envSchema = z.object({
-  NODE_ENV: z.enum(["development", "production"]).default("development"),
-
   BOT_TOKEN: z.string(),
 
   DEV_GUILD_ID: z.string(),
@@ -16,10 +15,18 @@ const envSchema = z.object({
 });
 
 export const safeEnv = () => {
-  const { success } = envSchema.safeParse(env);
+  const { success, error } = envSchema.safeParse(env);
 
   if (!success) {
-    throw new Error("NO SUCCESS U SUCK");
+    console.error("Invalid environment variables:");
+
+    error.format();
+
+    for (const issue of error.issues) {
+      console.error(`- ${issue.path.join(".")}: ${issue.message}`);
+    }
+
+    throw new Error("Failed to parse environment variables");
   }
 };
 
@@ -28,5 +35,5 @@ declare global {
     interface ProcessEnv extends z.infer<typeof envSchema> {}
   }
 
-  interface BunEnv extends z.infer<typeof envSchema> {}
+  interface Env extends z.infer<typeof envSchema> {}
 }
