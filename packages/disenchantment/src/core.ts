8324433ccec9ede75a2eef44ae5139322b54ce33
xmnlz <lemmeq9@gmail.com>
2025-03-24 23:03:10 +0100

    chore: remvod unused imports

--- packages/disenchantment/src/core.ts
+++ packages/disenchantment/src/core.ts
@@ -1,21 +1,13 @@
-import { Client, type ClientOptions, type Interaction } from "discord.js";
+import { Client, type ClientOptions } from "discord.js";
 import { type CommandOrCommandGroup } from "./command.js";
 import { flattenCommands, registerCommands } from "./command-register.js";
-import { executeInteraction } from "./interactiom-handler.js";
 import { MetadataStorage } from "./metadata-storage.js";
 
 export interface BotOptions {
   commands: CommandOrCommandGroup[];
   clientOptions: ClientOptions;
-  guildIds?: string[];
 }
 
-/**
- * Creates and boots up the Discord bot.
- *
- * @param options - Bot configuration, including commands and client options.
- * @returns An initialized Discord Client.
- */
 export async function createBot({
   clientOptions,
   commands,
