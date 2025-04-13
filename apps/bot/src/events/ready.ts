d76de26b720889095dffcf3901ded02be5184f30
xmnlz <lemmeq9@gmail.com>
2025-04-13 11:26:13 +0200

    chore: removed unused event for testing

--- apps/bot/src/events/ready.ts
+++
@@ -1,6 +0,0 @@
-import { EventBuilder } from "disenchantment/src";
-
-const onReadyEvent = new EventBuilder("ready").setHandler(([client]) => {
-  console.log("BOT IS READY??", client.isReady);
-  console.log("BOT IS RUNNING!");
-});
